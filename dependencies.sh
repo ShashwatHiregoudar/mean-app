98e364497034d547a393edfcc55e7519b7b0d599
Shashwat Hiregoudar <31088952+ShashwatHiregoudar@users.noreply.github.com>
2018-04-04 22:57:15 +0530

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,23 @@
+echo "SHashsawt Scrippt"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "Installing Mongobd"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+
+echo "Now Installing Node "
+echo " "
+echo " "
+echo " "
+sudo apt-get install python-software-properties
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs -y
+
