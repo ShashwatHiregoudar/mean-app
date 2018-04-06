15ba02bb89d07f7356592e610aeccbc5463db78e
ShashwatHiregoudar <shashwathiregoudar@gmail.com>
2018-04-06 22:22:17 +0530

    neatening the view

--- client/src/app/lawyer/lawyer.component.ts
+++ client/src/app/lawyer/lawyer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '../../../node_modules/@angular/core';
 import { LawyerService } from '../lawyer.service';
 import { Lawyer } from '../lawyer';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-lawyer',
   templateUrl: './lawyer.component.html',
@@ -17,10 +18,37 @@ export class LawyerComponent implements OnInit {
   slotdate: String;
   slottime: String;
   constructor(private lawyerService: LawyerService) { }
-
-  ngOnInit() {
+  addlawyer() {
+    const newLawyer = {
+      name : this.name,
+      type : this.type,
+      phone : this.phone,
+      slotdate : this.slotdate,
+      slottime : this.slottime
+    };
+    this.lawyerService.addLawyer(newLawyer)
+        .subscribe(lawyer => {
+          this.lawyers.push(lawyer);
+        });
+  }
+  deletelawyer(id) {
+    this.lawyerService.deleteLawyer(id)
+        .subscribe(data => {
+          if ( data.n === 1) {
+            for ( let i = 0; i < this.lawyers.length; i++) {
+              if ( this.lawyers[i]._id === id ) {
+                this.lawyers.splice(i, 1);
+              }
+            }
+          }
+        });
+  }
+  getlawyer() {
     this.lawyerService.getLawyers()
         .subscribe(lawyers => this.lawyers = lawyers);
+  }
+  ngOnInit() {
+    this.getlawyer();
   }
 
 }
